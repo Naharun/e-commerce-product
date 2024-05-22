46849381d4af37009a43cff67e3daaef4a57b898
Naharun <naharunsraboni@gmail.com>
2024-05-22 10:02:42 +0600

    created product management interface

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,8 @@ const app: Application = express();
 // const port = 3000
 
 //parsers
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
